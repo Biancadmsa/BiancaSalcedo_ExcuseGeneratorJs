95f70fd7a54cbf0e25695d5989e7080036e28ca6
Biancadmsa <biancadmsadf16@gmail.com>
2023-08-04 12:08:11 -0400

    miprimercommit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function generateexcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = [Math.floor(Math.random() * who.length)];
+  let randomAction = [Math.floor(Math.random() * action.length)];
+  let randomWhat = [Math.floor(Math.random() * what.length)];
+  let randomWhen = [Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    who[randomWho] + " " + action[randomAction] + " " + what[randomWhat] + " " + when[randomWhen];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
+
+  
